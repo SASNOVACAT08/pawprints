7d30fffc26d4f7471d774fddb1a2d3dc0c447d84
Ruben Barros <atosc08@gmail.com>
2023-05-21 23:00:58 +0200

    refactor(pawprints): datakitten to pawprints

--- apps/sdk-ts/src/logger.ts
+++ apps/sdk-ts/src/logger.ts
@@ -1,24 +1,24 @@
-export default class DataKittenLogger {
+export default class PawPrintsLogger {
   private static url = ''
   private static environment = ''
   private static projectId = ''
 
   static init(url: string, environment: string, projectId: string) {
-    DataKittenLogger.url = `${url}/api/log`
-    DataKittenLogger.environment = environment
-    DataKittenLogger.projectId = projectId
+    PawPrintsLogger.url = `${url}/api/log`
+    PawPrintsLogger.environment = environment
+    PawPrintsLogger.projectId = projectId
   }
 
   private static async fetch(level: string, title: string, content: string) {
     const body = JSON.stringify({
       title,
       level,
-      environment: DataKittenLogger.environment,
+      environment: PawPrintsLogger.environment,
       content,
-      projectId: DataKittenLogger.projectId,
+      projectId: PawPrintsLogger.projectId,
       createdAt: new Date(),
     })
-    fetch(DataKittenLogger.url, {
+    fetch(PawPrintsLogger.url, {
       method: 'POST',
       body,
       headers: {
@@ -28,19 +28,19 @@ export default class DataKittenLogger {
   }
 
   private static log(level: string, title: string, ...args: unknown[]) {
-    DataKittenLogger.fetch(level, title, JSON.stringify({ ...args }))
+    PawPrintsLogger.fetch(level, title, JSON.stringify({ ...args }))
     console.log(`[${level.toUpperCase()}] ${title}`, ...args)
   }
 
   static info(title: string, ...args: unknown[]) {
-    DataKittenLogger.log('info', title, ...args)
+    PawPrintsLogger.log('info', title, ...args)
   }
 
   static warn(title: string, ...args: unknown[]) {
-    DataKittenLogger.log('warn', title, ...args)
+    PawPrintsLogger.log('warn', title, ...args)
   }
 
   static error(title: string, ...args: unknown[]) {
-    DataKittenLogger.log('error', title, ...args)
+    PawPrintsLogger.log('error', title, ...args)
   }
 }
